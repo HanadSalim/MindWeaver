083227f843a62e614d5f5de8b2aeee1fe684013f
HanadSalim <45672173+HanadSalim@users.noreply.github.com>
2022-09-18 18:05:24 +0100

    add function complete

--- Phase1Project/src/FileDemo.java
+++ Phase1Project/src/FileDemo.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class FileDemo {
@@ -8,16 +9,11 @@ public class FileDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		welcome();
-		selector();
-		switch(optionSelected) {
-		case 1:
-			System.out.println("Reading files under the folder "+ folder.getAbsolutePath());
-			listFilesInFolder(folder);
-			break;
-		case 2:
-			System.out.println("Show other options");
-			break;
+		while(true) {
+			selector();
+			mainMenu(optionSelected);
 		}
+
 	}
 
 	private static void listFilesInFolder(File folder) {
@@ -34,15 +30,99 @@ public class FileDemo {
 	}
 
 	public static void welcome() {
-		System.out.println("******* WELCOME *******"
-		+"\nFileDemo by Hanad Salim"+"\n");
+		System.out.println("************* WELCOME *************"
+				+"\nFileDemo by Hanad Salim"+"\n");
 		System.out.println("Select one of the following "	
 				+ "\n1. List all available files"
-				+ "\n2. Add, Delete or Search");
+				+ "\n2. Add, Delete or Search"
+				+ "\n3. Close program");
 	}
+
 	public static void selector() {
 		Scanner x = new Scanner(System.in);
 		int input = x.nextInt();
-		optionSelected = input;
+		if(input==1||input==2||input==3) {
+			optionSelected = input;
+		}else{
+			System.out.println("Select either 1,2 or 3");
+			selector();
+		}
+	}
+
+	public static void mainMenu(int x) {
+		switch(x) {
+		case 1:
+			option1();
+			break;
+		case 2:
+			option2();
+			break;
+		case 3:
+			option3();
+		}
+	}
+
+	public static void option1() {
+		System.out.println("List of files stored under  "+ folder.getAbsolutePath());
+		listFilesInFolder(folder);	
+		try {
+			Thread.sleep(1000);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		System.out.println("\nSelect one of the following "	
+				+ "\n1. List all available files"
+				+ "\n2. Add, Delete or Search"
+				+ "\n3. Close program");
+	}
+
+	public static void option2() {
+		System.out.println("\nSelect one of the following "	
+				+ "\n1. Add"
+				+ "\n2. Delete"
+				+ "\n3. Search");
+		selector();
+		switch(optionSelected) {
+		case 1:
+			add();
+			break;
+		case 2:
+
+			break;
+		case 3:
+
+		}
+	}
+
+	public static void option3() {
+		System.out.println("************* Program terminated *************");
+		System.exit(0);
+	}
+
+	public static void add() {
+		System.out.println("Please enter file name");
+		Scanner x = new Scanner(System.in);
+		String input = x.next();
+		File file = new File(curDir+"/"+input+".txt");
+		try {
+			file.createNewFile();
+			System.out.println("file succesfully created");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		/*
+		 *  once it adds go back to main menu
+		 *  call welcome,selector and mainmenu
+		 */
+	}
+
+	public static void delete() {
+
+	}
+
+	public static void search() {
+
 	}
 }
